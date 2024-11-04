d320a4a276dff9088b0f45a9c6f837618d93fab5
René Mose Hansen <rmha63250@edu.ucl.dk>
2024-11-04 10:56:53 +0100

    User klasse i modellag 1. iteration

---
+++ El_Booking/El_Booking/Model/User.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace El_Booking.Model
+{
+    internal class User
+    {
+		private int userID;
+
+		public int UserID
+		{
+			get { return userID; }
+			set { userID = value; }
+		}
+
+
+		private string email;
+
+		public string Email
+		{
+			get { return email; }
+			set { email = value; }
+		}
+
+		private string telephoneNumber;
+
+		public string TelephoneNumber
+		{
+			get { return telephoneNumber; }
+			set { telephoneNumber = value; }
+		}
+
+		private string firstName;
+
+		public string FirstName
+		{
+			get { return firstName; }
+			set { firstName = value; }
+		}
+
+		private string lastName;
+
+		public string LastName
+		{
+			get { return lastName; }
+			set { lastName = value; }
+		}
+
+		private string password;
+
+		public string Password
+		{
+			get { return password; }
+			set { password = value; }
+		}
+
+		public User(string email, string telephoneNumber, string firstName, string lastName, string password = null) 
+		{ 
+			Email = email;
+			TelephoneNumber = telephoneNumber;
+			FirstName = firstName;
+			LastName = lastName;
+			Password = password;
+		}
+
+        public User(int userID, string email, string telephoneNumber, string firstName, string lastName) 
+			: this(email, telephoneNumber, firstName, lastName)
+        {
+            UserID = userID;
+			
+        }
+
+
+
+
+
+
+
+    }
+}
