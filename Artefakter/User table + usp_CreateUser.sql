b3100660d47b77bf862afa6e409699d94b04d799
Trollerolle <tjtr63311@edu.ucl.dk>
2024-11-04 13:57:22 +0100

    SQL til Database opstart

---
+++ Artefakter/User table + usp_CreateUser.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Users (
+UserID INT IDENTITY(1,1) PRIMARY KEY,
+FirstName NvarChar(50),
+LastName NvarChar(50),
+Email NvarChar(50),
+PhoneNumber NvarChar(50),
+Password NvarChar(21)
+)
+
+CREATE PROCEDURE usp_CreateUserAndLogin
+ @LoginName NvarChar(50),
+ @Password NvarChar(50),
+ @UserName NvarChar(50)
+ AS 
+ BEGIN 
+	BEGIN TRY
+
+	EXECUTE sp_addlogin @loginame = @LoginName, @passwd = @Password, @defdb = 'El_Booking';
+
+	EXECUTE sp_adduser @loginame = @LoginName, @name_in_db = @UserName;
+
+	EXECUTE sp_addrolemember @rolename = BookingUserRole, @membername = @UserName;
+
+	END TRY
+	BEGIN CATCH
+		PRINT 'An error has occurred:';
+		PRINT ERROR_MESSAGE();
+	END CATCH
+END;
+
+CREATE ROLE BookingUserRole;
