d99efc317175f80f09755a9cb4f3de2752d9474c
René Mose Hansen <rmha63250@edu.ucl.dk>
2024-11-18 13:40:55 +0100

    Using reposotoires i BookingViewModel

--- El_Booking/El_Booking/ViewModel/BookingViewModel.cs
+++ El_Booking/El_Booking/ViewModel/BookingViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using El_Booking.Model;
+using El_Booking.Model.Repositories;
 
 namespace El_Booking.ViewModel
 {
