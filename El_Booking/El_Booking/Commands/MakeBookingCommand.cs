07c41996f47d86f7dd30b83235457c1c922f04e7
Trollerolle <tjtr63311@edu.ucl.dk>
2024-12-17 10:14:07 +0100

    refak MakeBookingCommand

--- El_Booking/El_Booking/Commands/MakeBookingCommand.cs
+++ El_Booking/El_Booking/Commands/MakeBookingCommand.cs
@@ -51,7 +51,6 @@ namespace El_Booking.Commands
                 _storer.BookingRepository.Add(newBooking);
                 newBooking = _storer.BookingRepository.GetBy(newBooking.CarID.ToString());
                 _bookingViewModel.MainBookingViewModel.CurrentBooking = newBooking;
-                _bookingViewModel.GetCurrentDays(_bookingViewModel._startingDate.StartOfWeek());
 
                 MessageBox.Show($"Din booking er gennemført.", "Succes", MessageBoxButton.OK);
                 _bookingViewModel.MainBookingViewModel.SeeYourBookingCommand.Execute(parameter);
